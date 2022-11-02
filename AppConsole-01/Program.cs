e18deda494da1621c3cc18da70e4cc30aca1bcdb
adriano <adria@DESKTOP-SHK9A4G>
2022-11-01 22:09:06 -0400

    Corrigir

--- AppConsole-01/Program.cs
+++ AppConsole-01/Program.cs
@@ -3,7 +3,7 @@ using System.Drawing;
 
 string path = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../Imagem/ponei.jpeg"));
 
-int pixelsWidth = 200;
+int pixelsWidth = 150;
 
 string mPath = "C:\\Users\\adria\\Source\\Repos\\led-21\\AppConsole-01\\AppConsole-01\\Imagem\\ponei.jpeg";
 
@@ -13,7 +13,7 @@ int pixelsHeigth = (int)(img.Height / img.Width * pixelsWidth);
 
 Bitmap imagem = new Bitmap(img, pixelsWidth, pixelsHeigth);
 
-char[] charArr = { ' ', '#', '$', '%', '0', 'O', 'U', 'U', '-', ',', '.', ' ' };
+char[] charArr = { '.', ',', ',', ';', ';', '-', 'L', 'U', 'H', '#', '@' };
 
 
 for (int col = 0; col < imagem.Height; col++)
@@ -23,33 +23,13 @@ for (int col = 0; col < imagem.Height; col++)
         Color c = imagem.GetPixel(line, col);
         byte grayScale = (byte)((c.R * 0.3) + (c.G * 0.59) + (c.B * 0.11));
 
-        char caractere = charArr[(grayScale / 25) % 10];
+        char caractere = charArr[(grayScale / 25) % 11];
 
-      //  Colorir(caractere);
         Console.Write(caractere + " ");
-     //   Descolorir();
+
     }
     Console.WriteLine();
 }
-
-
-
-void Colorir(char c)
-{
-    if (c == 'H' || c == 'U')
-        Console.ForegroundColor = ConsoleColor.DarkRed;
-    if (c == 'L' || c == 'O' || c == '0')
-        Console.ForegroundColor = ConsoleColor.Red;
-    if (c == '-' || c == ',')
-        Console.ForegroundColor = ConsoleColor.Yellow;
-}
-
-void Descolorir()
-{
-    Console.ForegroundColor = ConsoleColor.White;
-}
-
-
 
 
 
